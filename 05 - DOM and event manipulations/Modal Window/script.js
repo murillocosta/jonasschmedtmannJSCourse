0c155f03582223c97e0d15a603af36dfb484e543
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-05-24 20:34:13 -0300

    game projects finished

--- 05 - DOM and event manipulations/Modal Window/script.js
+++ 05 - DOM and event manipulations/Modal Window/script.js
@@ -1,24 +1,29 @@
 'use strict';
 
- const modal = document.querySelector('.modal');
- const overlay = document.querySelector('.overlay');
- const btnCloseModal = document.querySelector('.close-modal');
- const btnsShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 
- const closeModal = function(){
+const closeModal = function () {
     modal.classList.add('hidden')
-    overlay.classList.add('hidden') 
+    overlay.classList.add('hidden')
 };
-const openModal = function(){
+const openModal = function () {
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden');
- };
+};
 
- for (let i = 0; i < btnsShowModal.length; i++){
-     btnsShowModal[i].addEventListener('click', openModal)
- };
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal)
+};
 
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
 
- btnCloseModal.addEventListener('click', closeModal);
- overlay.addEventListener('click', closeModal)
+    }
+});
