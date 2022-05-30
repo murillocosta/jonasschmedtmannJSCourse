fbf03179194ddcde51980effae4cd29d69e15354
murillo.costa@live.com <murillo.costa@live.com>
2022-05-29 21:04:20 -0300

    sunday studies

--- 09 - Data-Structures-Operators/script.js
+++ 09 - Data-Structures-Operators/script.js
@@ -12,6 +12,20 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address
+  }) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`)
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`)
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +41,61 @@ const restaurant = {
     },
   },
 };
+
+// DESTRUCTURING ARRAYS
+
+// receive 2 return values from a function
+// const [starter, mainCourse] = restaurant.order(2, 0)
+// console.log(starter, mainCourse)
+
+// nested destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested
+// console.log(i, j)
+// const [i, , [j, k]] = nested
+// console.log(i, j, k)
+
+// default values
+// const [p = 1, q = 1, r = 1] = [8, 9]
+// console.log(p, q, r)
+
+// OBJECT DESTRUCTURING
+
+const {
+  name,
+  openingHours,
+  categories
+} = restaurant
+console.log(name, openingHours, categories)
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags
+} = restaurant
+console.log(restaurantName, hours, tags)
+
+const {
+  menu = [], starterMenu: starters = []
+} = restaurant
+console.log(menu, starters)
+
+//nested objects
+const {
+  fri: {
+    open: o,
+    close: c
+  }
+} = openingHours
+console.log(o, c)
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2
+})
+
+const ingredients = [prompt('Let\'s make the pasta! Ingredient 1? '), prompt('Ingredient 2? '), prompt('Ingredient 3? ')]
+
+restaurant.orderPasta(...ingredients)
