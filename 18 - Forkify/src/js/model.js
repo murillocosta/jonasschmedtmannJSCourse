df91b76bb43764772d7ff4e67f6f1a948f8469df
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-07-11 15:11:45 -0300

    commit

--- 18 - Forkify/src/js/model.js
+++ 18 - Forkify/src/js/model.js
@@ -1,6 +1,4 @@
-import { async } from 'regenerator-runtime';
 import { API_URL, KEY, RES_PER_PAGE } from './config.js';
-// import { getJSON, sendJSON } from './helpers.js';
 import { AJAX } from './helpers.js';
 
 export const state = {
